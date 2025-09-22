04d0d6784f25607c8725e43783feac0ea0f78a2a
Lider Technology <lidertech.net@gmail.com>
2025-09-22 09:34:34 -0500

    Create enum.ts

---
+++ enum.ts
@@ -0,0 +1,11 @@
+// src/app/enums/states.enum.ts
+export enum StatesEnum {
+  DEFAULT = 'DEFAULT',
+  LOADING = 'LOADING',
+  SUCCESS = 'SUCCESS',
+  ERROR = 'ERROR',
+  PAGINATING = 'PAGINATING',
+  SUBMITTING = 'SUBMITTING',
+  VALIDATING = 'VALIDATING',
+  IDLE = 'IDLE',
+}
