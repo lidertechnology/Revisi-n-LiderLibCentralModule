f71e6d3a8151d4c2b42ca523aeeb181f94b7d961
Lider Technology <lidertech.net@gmail.com>
2025-09-22 09:35:17 -0500

    Create firebase-config.ts

---
+++ firebase-config.ts
@@ -0,0 +1,24 @@
+// src/app/config/firebase-config.ts
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+import { getAnalytics } from 'firebase/analytics';
+import { getMessaging } from 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
+export const messaging = getMessaging(app);
